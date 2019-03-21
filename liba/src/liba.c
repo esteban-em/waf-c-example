5747ef22176b95f16670040f50e941c020e80d4b
esteban-em <mercier.esteban@gmail.com>
2019-03-21 14:42:47 +0100

    initial commit

---
+++ liba/src/liba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "liba/liba.h"
+
+void liba_hello() {
+  fprintf(stdout, "%s\n", "hello from liba");
+}
+
+void liba_goodbye() {
+  fprintf(stdout, "%s\n", "hello from liba");
+}
+/* EOF */
